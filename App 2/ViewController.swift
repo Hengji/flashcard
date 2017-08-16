b5a87f26ca8ca05ed6ed1f8032106d339b80c10c
iD Student <idstudent@iDs-iMac.local>
2017-08-16 10:51:25 -0700

    this is my second commit

--- App 2/ViewController.swift
+++ App 2/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var qustionLabel: UILabel!
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        answerPickerView.dataSource = self;
+        answerPickerView.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setupCardUI() {
+        questionTextView.text = CardCollection.instance.currentCard.question
+        
+        qustionLabel.text = "Question \(CardCollection.instance.currentIndex + 1)/\(CardCollection.instance.cards.count)"
+        answerPickerView.reloadAllComponents()
+    }
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CardCollection.instance.currentCard.options.count
+    }
+    
+    // Pickerview delegates
+    // returns text of option at a given row.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CardCollection.instance.currentCard.options[row];
+    }
+    @IBAction func nextPlayer(_ sender: Any) {
+        var alert: UIAlertController
+        
+        if CardCollection.instance.checkAnswer(answerPickerview.selectedRow(errSecInternalComponent:0)){
+            alert = UIAlertController(title: "Correct", message: "Correct Answer!", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Yay!", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        else{
+        alert = UIAlertController(title: "Incorrect", message: "Incorrect Answer.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Aww.", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true)
+        }
+        
+        CardCollection.instance.nextQuestion()
+        
+        setupCardUI()
+        func shuffle<T>( list : [T]) -> [T] {
+            return GKRandomSource.sharedRandom().arrayByShufflingObjects(in: list) as! [T]+
+    }
 
 }
 
